06412239ab1b00a320ae8e9be9df801cef0912d4
Rainerino <albertyanyy@gmail.com>
2019-10-17 12:52:21 -0700

    completed the docker build and push

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,22 @@
-FROM node:10
-WORKDIR /app
-COPY package.json /app
+# Use node 10.16.3 locally
+FROM node:10.16.3-stretch-slim
+
+RUN mkdir -p /home/node/app/node_modules && chown -R node:node /home/node/app
+
+WORKDIR /home/node/app
+# Copy source code
+COPY package*.json ./
+
+# we want to run the code in user mode. 
+USER node
+# Install dependencies
 RUN npm install
-COPY . /app
-CMD node app.js
-EXPOSE 8081
+
+# change the permission of the application code
+COPY --chown=node:node . .
+
+# Expose API port to the outside
+EXPOSE 8080
+
+# Launch application
+CMD ["npm","start"]
--- app.js
+++ app.js
@@ -1,29 +1,38 @@
-//Load express module with `require` directive
-var express = require('express')
-var mysql = require('mysql');
-
-var app = express()
-
-var con = mysql.createConnection({
-  host: "ss",
-  user: "admin",
-  password: ""
-});
-
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+// This will be our application entry. We'll setup our server here.
+const http = require('http');
+// Set up the express app
+const app = express();
+// Log requests to the console.
+app.use(logger('dev'));
+// Parse incoming requests data (https://github.com/expressjs/body-parser)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// Setup a default catch-all route that sends back a welcome message in JSON format.
 
 
+const port = parseInt(process.env.PORT, 10) || 8000;
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port);
+
+module.exports = app;
+
+app.get('/', (req, res) => res.status(200).send({
+
+  message: 'Hello World.',
+
+}));
+
+var amount = 5
+
+app.get('/wat', (req, res) => res.status(200).send({
+  message: 'Welcome to the beginning of nothingness.',
+  test: 'where is $(amount) tips'
+}));
 
 
-//Define request response in root URL (/)
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
-
-//Launch listening server on port 8081
-app.listen(8081, function () {
-  console.log('app listening on port 8081!')
-})
