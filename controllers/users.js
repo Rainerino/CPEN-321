b80c878d4e232146f46e856def05a3bb732e32ab
unknown <nimanasiri2525@hotmail.com>
2019-11-01 20:31:04 -0700

    Rough version of Google Calendar now working

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,13 @@
+const fs = require('fs'); //NOT NECESSARY, for calendar testing
 const JWT = require('jsonwebtoken');
 const User = require('../models/user');
-const { JWT_SECRET } = require('../configuration');
+const { JWT_SECRET, oauth } = require('../configuration');
+const { google } = require('googleapis');
+const googleAuth = require('google-auth-library');
+
+
+const TOKEN_PATH = 'calendar-nodejs-quickstart.json';
+const REDIRECT_URL = "http://localhost:3000/users/oauth/google";
 
 // This is the payload
 signToken = (user) => {
@@ -46,6 +53,39 @@ module.exports = {
     },
 
     googleOAuth: async(req, res, next) => {
+        // Going through oauth to get calendar
+        const client_id = oauth.google.clientID;
+        const client_secret = oauth.google.clientSecret;
+
+        var oauth2Client = new googleAuth.OAuth2Client(
+            client_id,
+            client_secret,
+            REDIRECT_URL
+        );
+
+        const google_token = fs.readFileSync(TOKEN_PATH);
+        oauth2Client.setCredentials(JSON.parse(google_token));
+
+        // Got calendar
+        var calendar = google.calendar('v3');
+
+        calendar.events.list({
+            auth: oauth2Client,
+            calendarId: "nimanasirisoccerguy@gmail.com", //Need to grab a calendar ID
+            timeMin: "2019-11-01T00:00:00.000Z", 
+            timeMax: "2019-11-01T23:59:59.000Z"
+        }, function(err, response) {
+            if (err) {
+                console.log('The API returned an error: ' + err);
+                return;
+            }
+            var events = response.data.items;
+            events.forEach(function(event) {
+                var start = event.start.dateTime || event.start.date;
+                console.log('%s - %s', start, event.summary);
+            });
+        });
+
         // Generate token
         const token = signToken(req.user);
         res.status(200).json({ token });
