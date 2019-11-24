3efc86c2590ff09bffd3143f1c6ec99788a5eec3
Rainerino <albertyanyy@gmail.com>
2019-11-24 10:39:55 -0800

    updated some requests

--- routes/calendar.js
+++ routes/calendar.js
@@ -14,13 +14,16 @@ router.use((req, res, next) => {
 router.get('/:calendarId', calendarController.getCalendar);
 // add events, if event doesn't exist just create one
 router.put('/event/add-events', calendarController.putEvent);
-router.delete('/:calendarId/event/delete/:eventId', calendarController.deleteEvent); // delete calendar event
+// delete calendar event
+router.delete('/:calendarId/event/delete/:eventId', calendarController.deleteEvent);
+// get events of a certain time slot. This will not update the calendar
 router.get('/:calendarId/event/time-slices', calendarController.getCalendarTimeSlot);
-// TODO: date based time slices
+// return all of the events
 router.get('/:calendarId/event/all', calendarController.getAllCalendarEvents);
+// return the calendar events of today, it will update the event
 router.get('/:calendarId/event/today', calendarController.getTodayEvents);
-router.post('/create', calendarController.createCalendar); // create a calendar // fIXME remove this
-router.put('/:calendarId/combine-calendar', calendarController.combineCalendar); // add a calendar to another. // FIXME remove
+
+router.put('/:calendarId/combine-calendar', calendarController.combineCalendar);
 // TODO: group ill have different request
 // /:calendarId/group/event/all'
 
